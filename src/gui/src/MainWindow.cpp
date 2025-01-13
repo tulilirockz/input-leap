3d4d13eceef41ebf3e9fcc8b07578c92bdcf74b0
Tulip Blossom <tulilirockz@outlook.com>
2025-01-13 09:45:19 -0300

    feat: use XDG path for default configuration location

--- src/gui/src/MainWindow.cpp
+++ src/gui/src/MainWindow.cpp
@@ -17,7 +17,6 @@
  */
 
 #include <iostream>
-
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
@@ -46,6 +45,7 @@
 #include <QFileDialog>
 #include <QDesktopServices>
 #include <QRegularExpression>
+#include <QStandardPaths>
 
 #if defined(Q_OS_MAC)
 #include <ApplicationServices/ApplicationServices.h>
@@ -328,7 +328,7 @@ void MainWindow::loadSettings()
 
     ui_->m_pGroupServer->setChecked(settings().value("groupServerChecked", false).toBool());
     ui_->m_pLineEditConfigFile->setText(settings().value("configFile",
-                                                    QDir::homePath() + "/" + APP_CONFIG_NAME).toString());
+                                                    QStandardPaths::ConfigLocation + "/" + APP_CONFIG_NAME).toString());
     ui_->m_pGroupClient->setChecked(settings().value("groupClientChecked", true).toBool());
     ui_->m_pLineEditHostname->setText(settings().value("serverHostname").toString());
 }
